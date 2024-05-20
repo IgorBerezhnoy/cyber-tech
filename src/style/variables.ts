fe5b33f5f777d1ad096a223df02cf5b22d147f4f
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-05-20 21:36:08 +0800

    [feat]: created variables

---
+++ src/style/variables.ts
@@ -0,0 +1,30 @@
+//colors
+export const COLOR_GRAY = `#9394AA`
+export const COLOR_WHITE = `#FFFFFF`
+export const COLOR_BLACK = `#2E2E36`
+export const COLOR_MAIN = `#228B62`
+export const COLOR_RED = `#FF4B24`
+export const COLOR_GRAY_LIGHT = `#fdfdfd`
+
+// font size
+export const FONTSIZE_XS = `10px`
+export const FONTSIZE_S = `12px`
+export const FONTSIZE_M = `14px`
+export const FONTSIZE_L = `16px`
+export const FONTSIZE_XL = `18px`
+export const FONTSIZE_XXL = `24px`
+export const FONTSIZE_XXXL = `32px`
+export const FONTSIZE_XXXXL = `48px`
+
+//font-weight
+export const FONT_WEIGHT_NORMAL = `400`
+export const FONT_WEIGHT_MEDIUM = `500`
+export const FONT_WEIGHT_MEDIUM_PLUS = `600`
+export const FONT_WEIGHT_BOLD = `700`
+
+//font-family
+export const FONT_FAMILY = `Jost, Jost Variable, sans-serif`
+
+//line-height
+export const LINE_HEIGHT_M = `18px`
+export const LINE_HEIGHT_L = `24px`
